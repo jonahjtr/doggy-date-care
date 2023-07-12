1ab1204b43ffa7cf01040faaddd14ad4d388f57e
jonah.jtr <tillmanjonah@gmail.com>
2023-07-12 18:36:09 -0500

    changin idea, going to do landing page to sell and have seperate login and create acconunt

--- src/pages/landing/LandingPage.js
+++ src/pages/landing/LandingPage.js
@@ -1,21 +1,6 @@
-import { Link } from "react-router-dom";
-import doggy from "../../media/landing-page-picture.jpg";
 import "./LandingPage.css";
 function LandingPage() {
-  return (
-    <div className="landing-page">
-      <div className="left-side">
-        <img className="left-side-photo" src={doggy} alt="doggo" />
-      </div>
-      <div className="right-side">
-        <div className="logo"> logo goes here</div>
-        <div className="form"> form goes here</div>
-        <div className="login-link">
-          already have an account? <Link to={"/login"}>login</Link>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="landing-page">landing</div>;
 }
 
 export default LandingPage;
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,32 @@
+import { Link } from "react-router-dom";
+import doggy from "../../media/landing-page-picture.jpg";
+import "./Login.css";
+
 function Login() {
-  return <div>login page</div>;
+  return (
+    <div>
+      <div className="landing-page">
+        <div className="left-side">
+          <img className="left-side-photo" src={doggy} alt="doggo" />
+        </div>
+        <div className="right-side">
+          <div className="logo"> logo goes here</div>
+          <div className="form">
+            <div>Log in!</div>
+            <form>
+              <label>Email:</label>
+              <input type="text" />
+              <label>Password:</label>
+              <input type="text" />
+              <button>Login</button>
+            </form>
+          </div>
+          <div className="login-link">
+            Dont have an account? <Link to={"/login"}>CreateAccount</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Login;
