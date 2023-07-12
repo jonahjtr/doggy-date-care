d2a7f94077ebca6bf893886cc0e9300a883d5061
jonah.jtr <tillmanjonah@gmail.com>
2023-07-12 18:23:33 -0500

    adding image for landing page and editing to fit

--- src/pages/landing/LandingPage.js
+++ src/pages/landing/LandingPage.js
@@ -1,19 +1,19 @@
 import { Link } from "react-router-dom";
-
+import doggy from "../../media/landing-page-picture.jpg";
+import "./LandingPage.css";
 function LandingPage() {
   return (
-    <div className="Landing-page">
-      <ul>
-        <li>
-          {" "}
-          <Link to="/login">Login</Link>{" "}
-        </li>
-        <li>
-          {" "}
-          <Link to="/create-account">create account</Link>
-        </li>
-        <li> </li>
-      </ul>
+    <div className="landing-page">
+      <div className="left-side">
+        <img className="left-side-photo" src={doggy} alt="doggo" />
+      </div>
+      <div className="right-side">
+        <div className="logo"> logo goes here</div>
+        <div className="form"> form goes here</div>
+        <div className="login-link">
+          already have an account? <Link to={"/login"}>login</Link>
+        </div>
+      </div>
     </div>
   );
 }
